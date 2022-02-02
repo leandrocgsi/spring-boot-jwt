04e4a82a87bd419b778325e83662201c5b174d9f
leandrocgsi <leandrocgsi@gmail.com>
2022-02-01 21:46:40 -0300

    Initial Commit

--- src/main/java/br/com/erudio/Startup.java
+++ src/main/java/br/com/erudio/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJwtApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJwtApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
